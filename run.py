ca17b86d27e01f5080a5afcfa27ef4b8f2a57e5e
bhavya20428 <bhavya20428@iiitd.ac.in>
2022-04-27 03:52:23 +0530

    DIYA

--- run.py
+++ run.py
@@ -1,14 +1,36 @@
-from flask import Flask, url_for,redirect,render_template
+from crypt import methods
+from flask import Flask, url_for,redirect,render_template,request
 from flask_mysqldb import MySQL
 import yaml
 
 app=Flask(__name__)
 
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
 
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def home():
+    if request.method == 'POST':
+        
+        customerlogin = request.form
+        mobileno= customerlogin['mobileno']
+        pwd = customerlogin['pwd']
+
+        cur = mysql.connection.cursor()
+        check = cur.execute("SELECT * FROM tablename where mobileno="+mobileno+" and pwd="+pwd)
+        if check>0:
+            return redirect(url_for())
+        
+        cur.close()
+       
     return render_template('index.html')
+   
 
 
 if __name__=='__main__':
