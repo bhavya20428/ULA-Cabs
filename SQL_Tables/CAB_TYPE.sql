7eb44e6d31e0dd4ab27c954899648c8a18f92d8d
Diya Ahuja <diya20431@iiitd.ac.in>
2022-05-01 23:35:11 +0530

    Add files via upload

---
+++ SQL_Tables/CAB_TYPE.sql
@@ -0,0 +1,6 @@
+insert into CAB_TYPE (typeName, cost, numberOfSeats) values ('micro', 50, 4);
+insert into CAB_TYPE (typeName, cost, numberOfSeats) values ('mini', 60, 4);
+insert into CAB_TYPE (typeName, cost, numberOfSeats) values ('Prime Sedan', 75, 5);
+insert into CAB_TYPE (typeName, cost, numberOfSeats) values ('Prime SUV', 100, 6);
+insert into CAB_TYPE (typeName, cost, numberOfSeats) values ('Auto', 40, 3);
+insert into CAB_TYPE (typeName, cost, numberOfSeats) values ('Bike', 30, 1);
