a0f5dc880a6b34dabf4c2e3af5ad92b8291415b0
diyaahuja <diya20431@iiitd.ac.in>
2022-05-01 13:11:15 +0530

    profile

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ app=Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '12345'
-# app.config['MYSQL_PASSWORD'] = 'Diya@1204'
+# app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_PASSWORD'] = 'Diya@1204'
 app.config['MYSQL_DB'] = 'ULA'
 mysql = MySQL(app)
 
@@ -35,7 +35,7 @@ def clogin():
         for i in rows:
             if (mobileno == i[0]) and (pwd == i[1]):
                 
-                return render_template('userportal.html')
+                return redirect(url_for('ulogged', phoneno=mobileno,password=pwd))
         
         cur.close()
        
@@ -51,7 +51,7 @@ def dlogin():
 
         cur = mysql.connection.cursor()
         
-        cur.execute("SELECT phoneNo,pwd FROM DRIVER where phoneNo="+mobileno)
+        cur.execute("SELECT phoneNo,password FROM DRIVER where phoneNo="+mobileno)
         rows=cur.fetchall()
         
         for i in rows:
@@ -59,7 +59,7 @@ def dlogin():
                 arr={mobileno,pwd}
                 
                 # return redirect(url_for('driverlogged'))
-                return redirect(url_for('dlogged', phoneno=mobileno,pwd=pwd))
+                return redirect(url_for('dlogged', phoneno=mobileno,password=pwd))
         
         cur.close()
        
@@ -68,14 +68,26 @@ def dlogin():
 @app.route('/driverlogged')
 def dlogged():
     phoneno= request.args['phoneno']
-    pwd=request.args['pwd']
+    pwd=request.args['password']
     cur = mysql.connection.cursor()
-    cur.execute('Select name,age from Driver where phoneNo=%s and pwd=%s;',(phoneno,pwd))
+    cur.execute('Select driverID, name, age, gender, phoneNo, totalAmountEarned, ratings, vehicleID from Driver where phoneNo=%s and password=%s;',(phoneno,pwd))
     rows=cur.fetchone()
     cur.close()
     
 
-    return render_template("driverprofile.html",name=rows[0],age=rows[1])
+    return render_template("driverprofile.html",driverID=rows[0], name=rows[1],age=rows[2], gender=rows[3],phoneNo=rows[4],totalAmountEarned=rows[5],ratings=rows[6],vehicleID=rows[7])
+
+@app.route('/userlogged')
+def ulogged():
+    phoneno= request.args['phoneno']
+    pwd=request.args['password']
+    cur = mysql.connection.cursor()
+    cur.execute('Select userID, name, phoneNo , age, gender from User where phoneNo=%s and pwd=%s;',(phoneno,pwd))
+    rows=cur.fetchone()
+    cur.close()
+    
+
+    return render_template("profile.html", userID=rows[0], name=rows[1],phoneNo=rows[2] ,age=rows[3], gender=rows[4])
 
 
 
@@ -97,13 +109,13 @@ def dsign():
         cur.execute("select count(*) from USER")
         num=str(cur.fetchall()[0][0]+1)
 
-        cur.execute("select vehicleID from Vehicle not in(select vehicleID from DRIVER")
-        vehicleID=cur.fetchall()[0][0]
+        # cur.execute("select vehicleID from Vehicle not in(select vehicleID from DRIVER")
+        # vehicleID=cur.fetchall()[0][0]
 
 
         
         gender=gender[:1]
-        cur.execute("insert into DRIVER (driverID, name, age, gender, phoneNo, totalAmountEarned, ratings, vehicleID, pwd) values (%s, %s, %s, %s, %s, '1', '1',%s , %s)",(num,name,age,gender,phone,vehicleID,pwd))
+        cur.execute("insert into DRIVER (driverID, name, age, gender, phoneNo, totalAmountEarned, ratings, vehicleID, password) values (%s, %s, %s, %s, %s, '1', '1',%s , %s)",(1011,name,age,gender,phone,3,pwd))
 
 
         mysql.connection.commit()
